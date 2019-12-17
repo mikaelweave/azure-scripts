f2818917702b6c301140e0d1974cb163c91f986f
Mikael Weaver <mikael.weaver@microsoft.com>
2019-12-17 07:11:42 -0800

    Fixed bugs

--- adf-storage-move/main.tf
+++ adf-storage-move/main.tf
@@ -1,6 +1,7 @@
 provider "azurerm" {
-  version = "~>1.19"
+  version = "~>1.39.0"
 }
+data "azurerm_subscription" "current" {}
 
 variable "resourceGroupName" {
   type        = string
@@ -38,53 +39,67 @@ resource "azurerm_resource_group" "main" {
 
 resource "azurerm_storage_account" "source" {
   name                     = var.sourceStorageAccountName
-  resource_group_name      = var.resourceGroupName
+  resource_group_name      = azurerm_resource_group.main.name
   location                 = var.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-  account_kind             = "BlobStorage"
 }
 
 resource "azurerm_storage_container" "source_container" {
   name                  = var.sourceContainerName
-  resource_group_name   = var.resourceGroupName
-  storage_account_name  = var.sourceStorageAccountName
+  storage_account_name  = azurerm_storage_account.source.name
   container_access_type = "private"
 }
 
 resource "azurerm_storage_account" "dest" {
   name                     = var.destStorageAccountName
-  resource_group_name      = var.resourceGroupName
+  resource_group_name      = azurerm_resource_group.main.name
   location                 = var.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-  account_kind             = "BlobStorage"
 }
 
 resource "azurerm_storage_container" "dest_container" {
-  name                  = var.destStorageAccountName
-  resource_group_name   = var.resourceGroupName
-  storage_account_name  = var.destStorageAccountName
+  name                  = var.destContainerName
+  storage_account_name  = azurerm_storage_account.dest.name
   container_access_type = "private"
 }
 
 resource "azurerm_data_factory" "adf" {
   name                = var.factoryName
   location            = var.location
-  resource_group_name = var.resourceGroupName
+  resource_group_name = azurerm_resource_group.main.name
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+resource "azurerm_role_assignment" "source-mi" {
+  scope                = "/subscriptions/${data.azurerm_subscription.current.subscription_id}/resourceGroups/${azurerm_resource_group.main.name}/providers/Microsoft.Storage/storageAccounts/${azurerm_storage_account.source.name}/blobServices/default/containers/${azurerm_storage_container.source_container.name}"
+  role_definition_name = "Reader"
+  principal_id         = lookup(azurerm_data_factory.adf.identity[0], "principal_id")
+}
+
+resource "azurerm_role_assignment" "dest-mi" {
+  scope                = "/subscriptions/${data.azurerm_subscription.current.subscription_id}/resourceGroups/${azurerm_resource_group.main.name}/providers/Microsoft.Storage/storageAccounts/${azurerm_storage_account.dest.name}/blobServices/default/containers/${azurerm_storage_container.dest_container.name}"
+  role_definition_name = "Reader"
+  principal_id         = lookup(azurerm_data_factory.adf.identity[0], "principal_id")
 }
 
 resource "azurerm_template_deployment" "data-factory-dependencies" {
-  name                = "MyApp-ARM"
+  name                = "adfComponentsDeployment"
   resource_group_name = azurerm_resource_group.main.name
 
   template_body = file("azuredeploy.json")
 
   parameters = {
     "factoryName" = var.factoryName,
-    "blobSourceAccountUrl" = "https://${var.sourceStorageAccountName}.blob.core.windows.net/",
-    "blobDestAccountUrl" = "https://${var.destStorageAccountName}.blob.core.windows.net/"
+    "blobSourceAccountUrl" = "https://${azurerm_storage_account.source.name}.blob.core.windows.net/",
+    "blobDestAccountUrl" = "https://${azurerm_storage_account.dest.name}.blob.core.windows.net/".
+    "srcContainerName" = var.sourceContainerName,
+    "destContainerName" = var.destContainerName
   }
 
   deployment_mode = "Incremental"
+  depends_on = [azurerm_data_factory.adf, azurerm_storage_container.source_container, azurerm_storage_container.dest_container]
 }
