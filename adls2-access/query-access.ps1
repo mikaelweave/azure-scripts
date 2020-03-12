2615c64ba6333f0e3fe925ba3cb5ef0e171fe8c2
Mikael Weaver <mikael.weaver@microsoft.com>
2020-03-11 17:35:57 -0700

    Commiting in progress SQL changes

--- adls2-access/query-access.ps1
+++ adls2-access/query-access.ps1
@@ -13,10 +13,78 @@ $StorageAccountName = "adls2accesstest"
 Set-AzContext -Subscription $SubscriptionId
 
 # Get all RBAC roles with data access
-$RolesWithAccess = $(Get-AzRoleDefinition | Where-Object -FilterScript {$_.dataActions -like "Microsoft.Storage/storageAccounts/blobServices/containers/blobs/*"})
+$RolesWithDataAccess = $(Get-AzRoleDefinition | Where-Object -FilterScript {$_.dataActions -like "Microsoft.Storage/storageAccounts/blobServices/containers/blobs/*"})
+$KeyAccessActionArray = "*", "Microsoft.Storage/storageAccounts/*", "Microsoft.Storage/storageAccounts/listkeys/action", "Microsoft.Storage/storageAccounts/blobServices/generateUserDelegationKey/action"
+$RolesWithKeyAccess = $()
+foreach ($action in $KeyAccessActionArray) {
+    $RolesWithKeyAccess = $RolesWithKeyAccess + $(Get-AzRoleDefinition | Where-Object -FilterScript {$_.Actions.Contains($action)})
+}
 
 # Get resource ID
 $StorageAccount = $(Get-AzStorageAccount | Where-Object StorageAccountName -eq $StorageAccountName)
 
 # Get role assignments for storage account (and child containers)
-Get-AzRoleAssignment -Scope $StorageAccount.Id | Where-Object RoleDefinitionId -in $RolesWithAccess.Id
+$AssignmentsWithDataAccess = $(Get-AzRoleAssignment -Scope $StorageAccount.Id | Where-Object RoleDefinitionId -in $RolesWithDataAccess.Id)
+$AssignmentsWithKeyAccess = $(Get-AzRoleAssignment -Scope $StorageAccount.Id | Where-Object RoleDefinitionId -in $RolesWithKeyAccess.Id)
+
+# Export to files
+$AssignmentsWithDataAccess | export-csv AssignmentsWithDataAccess.csv -notypeinformation
+$AssignmentsWithKeyAccess | export-csv AssignmentsWithKeyAccess.csv -notypeinformation
+
+$Token = Get-AzCachedAccessToken
+$Filesystems = Get-ADLSContainers -StorageAccountName $StorageAccountName -BearerToken $Token
+
+function Get-ADLSContainers()
+{
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory = $true, Position = 1)] [string] $StorageAccountName,
+        [Parameter(Mandatory = $true, Position = 1)] [string] $BearerToken
+    )
+    # Rest documentation:
+    # https://docs.microsoft.com/en-us/rest/api/storageservices/datalakestoragegen2/path/create
+    $method = "GET"
+ 
+    $headers = @{ } 
+    $headers.Add("x-ms-version", "2018-11-09")
+    $headers.Add("Authorization", "Bearer $BearerToken")
+    $headers.Add("If-None-Match", "*") # To fail if the destination already exists, use a conditional request with If-None-Match: "*"
+ 
+    $URI = "https://$StorageAccountName.dfs.core.windows.net/?resource=account"
+ 
+    try {
+        $result = Invoke-RestMethod -method $method -Uri $URI -Headers $headers # returns empty response
+        $result
+    }
+    catch {
+        $ErrorMessage = $_.Exception.Message
+        $StatusDescription = $_.Exception.Response.StatusDescription
+ 
+        Throw $ErrorMessage + " " + $StatusDescription + " PathToCreate: $PathToCreate"
+    }
+}
+
+function Get-AzCachedAccessToken()
+{
+    $ErrorActionPreference = 'Stop'
+  
+    if(-not (Get-Module Az.Accounts)) {
+        Import-Module Az.Accounts
+    }
+    $azProfile = [Microsoft.Azure.Commands.Common.Authentication.Abstractions.AzureRmProfileProvider]::Instance.Profile
+    if(-not $azProfile.Accounts.Count) {
+        Write-Error "Ensure you have logged in before calling this function."    
+    }
+  
+    $currentAzureContext = Get-AzContext
+    $profileClient = New-Object Microsoft.Azure.Commands.ResourceManager.Common.RMProfileClient($azProfile)
+    Write-Debug ("Getting access token for tenant" + $currentAzureContext.Tenant.TenantId)
+    $token = $profileClient.AcquireAccessToken($currentAzureContext.Tenant.TenantId)
+    $token.AccessToken
+}
+
+function Get-AzBearerToken()
+{
+    $ErrorActionPreference = 'Stop'
+    ('Bearer {0}' -f (Get-AzCachedAccessToken))
+}
--- data-pipeline/setupSql.ps1
+++ data-pipeline/setupSql.ps1
@@ -1,65 +1,73 @@
 Param(
     [string]$ServicePrincipalId,
-    [string]$ServicePrincipalPassword,
+    [SecureString]$ServicePrincipalPassword,
     [string]$TenantId,
     [string]$SQLServerName,
-    [string]$userObjectId,
-    [string]$userName
+    [string]$SQLDatabaseName,
+    [string]$GrantServicePrincipalName,
+    [string]$GrantServicePrincipalObjectId
 )
 
 
 # https://www.c-sharpcorner.com/blogs/create-azure-service-principal-and-get-aad-auth-token
-Function GetAuthTokenInvokingRestApi {  
-    Param(  
-       [Parameter(Mandatory)][ValidateNotNull()][ValidateNotNullOrEmpty()]  
-       [String]$TenantID,  
-       [Parameter(Mandatory)][ValidateNotNull()][ValidateNotNullOrEmpty()]  
-       [String]$ServicePrincipalId,  
-       [Parameter(Mandatory)][ValidateNotNull()][ValidateNotNullOrEmpty()]  
-       [String]$ServicePrincipalPwd,  
-       [Parameter(Mandatory)][ValidateNotNull()][ValidateNotNullOrEmpty()]  
-       [string]$ApiEndpointUri  
- )  
- $encodedSecret = [System.Web.HttpUtility]::UrlEncode($ServicePrincipalPwd)  
- $RequestAccessTokenUri = "https://login.microsoftonline.com/$TenantID/oauth2/token"  
- $body = "grant_type=client_credentials&client_id=$ServicePrincipalId&client_secret=$encodedSecret&resource=$ApiEndpointUri"  
- $contentType = 'application/x-www-form-urlencoded'  
- 
- try {  
-    $Token = Invoke-RestMethod -Method Post -Uri $RequestAccessTokenUri -Body $body -ContentType $contentType    
-    }  
-    catch { throw }  
-  $Token
- } 
+Function GetAuthTokenInvokingRestApi {
+    Param(
+        [Parameter(Mandatory)][ValidateNotNull()][ValidateNotNullOrEmpty()]  
+        [String]$TenantID,
+        [Parameter(Mandatory)][ValidateNotNull()][ValidateNotNullOrEmpty()]  
+        [String]$ServicePrincipalId,
+        [Parameter(Mandatory)][ValidateNotNull()][ValidateNotNullOrEmpty()]  
+        [SecureString]$ServicePrincipalPwd,
+        [Parameter(Mandatory)][ValidateNotNull()][ValidateNotNullOrEmpty()]  
+        [string]$ApiEndpointUri
+    )
+    $credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $ServicePrincipalId, $ServicePrincipalPwd
+    $encodedSecret = [System.Web.HttpUtility]::UrlEncode($credential.GetNetworkCredential().password)
+    $RequestAccessTokenUri = "https://login.microsoftonline.com/$TenantID/oauth2/token"
+    $body = "grant_type=client_credentials&client_id=$ServicePrincipalId&client_secret=$encodedSecret&resource=$ApiEndpointUri"  
+    $contentType = 'application/x-www-form-urlencoded'
 
- 
- # https://blog.bredvid.no/handling-azure-managed-identity-access-to-azure-sql-in-an-azure-devops-pipeline-1e74e1beb10b
+    try {
+        $Token = Invoke-RestMethod -Method Post -Uri $RequestAccessTokenUri -Body $body -ContentType $contentType    
+    }
+    catch { throw }
+    $Token
+}
+
 Function ConvertTo-Sid {
     param (
-        [string]$appId
+        [string]$objectId
     )
-    [guid]$guid = [System.Guid]::Parse($appId)
+    [guid]$guid = [System.Guid]::Parse($objectId)
     foreach ($byte in $guid.ToByteArray()) {
         $byteGuid += [System.String]::Format("{0:X2}", $byte)
     }
     return "0x" + $byteGuid
 }
- 
-  GetAuthTokenInvokingRestApi -TenantID $TenantId -ServicePrincipalId $ServicePrincipalId -ServicePrincipalPwd $ServicePrincipalPassword -ApiEndpointUri "https://database.windows.net/" -OutVariable SPNToken
 
- Write-Verbose "Create SQL connectionstring"
- $conn = New-Object System.Data.SqlClient.SQLConnection 
- $DatabaseName = 'Master'
- $conn.ConnectionString = "Data Source=$SQLServerName.database.windows.net;Initial Catalog=$DatabaseName;Connect Timeout=30"
- $conn.AccessToken = $($SPNToken.access_token)
- $conn
- 
- Write-Verbose "Connect to database and execute SQL script"
- $sid = ConvertTo-Sid "57b45b2d-27ee-41dc-b1ae-70b1bc83030c"
- $conn.Open() 
- $query = "DROP USER IF EXISTS $userName; CREATE USER [$userName] WITH DEFAULT_SCHEMA=[dbo], SID = $sid, TYPE = E; ALTER ROLE db_owner ADD MEMBER [$userName]"
- #$query = 'select @@version'
- $command = New-Object -TypeName System.Data.SqlClient.SqlCommand($query, $conn)     
- $Result = $command.ExecuteScalar()
- $Result
- $conn.Close() 
+$sid = ConvertTo-Sid $GrantServicePrincipalObjectId
+Write-Host $sid
+
+GetAuthTokenInvokingRestApi -TenantID $TenantId -ServicePrincipalId $ServicePrincipalId -ServicePrincipalPwd $ServicePrincipalPassword -ApiEndpointUri "https://database.windows.net/" -OutVariable SPNToken
+
+$conn = New-Object System.Data.SqlClient.SQLConnection 
+$conn.ConnectionString = "Data Source=$SQLServerName.database.windows.net;Initial Catalog=$SQLDatabaseName;Connect Timeout=30"
+$conn.AccessToken = $($SPNToken.access_token)
+
+$conn.Open() 
+#$query = "IF NOT EXISTS (SELECT * FROM sys.database_principals WHERE name = N'$GrantServicePrincipalName') CREATE USER [$GrantServicePrincipalName] FROM EXTERNAL PROVIDER; EXEC sp_addrolemember 'db_owner', '$userName';"
+#$query = "IF NOT EXISTS (SELECT * FROM sys.database_principals WHERE name = N'$GrantServicePrincipalName') CREATE USER [$GrantServicePrincipalName] SID = [0x2D5BB457EE27DC41B1AE70B1BC83030C], TYPE = E;; EXEC sp_addrolemember 'db_owner', '$userName';"
+$query = @"
+	DECLARE @username VARCHAR(60)
+	SET @username = '$GrantServicePrincipalName'
+	DECLARE @stmt VARCHAR(MAX)
+	SET @stmt = '
+	IF NOT EXISTS(SELECT 1 FROM sys.database_principals WHERE name =''' + @username +''')
+	BEGIN
+		CREATE USER [' + @username + '] WITH DEFAULT_SCHEMA=[dbo], SID = $sid, TYPE = E;
+	END'
+	PRINT(@stmt)
+"@
+$command = New-Object -TypeName System.Data.SqlClient.SqlCommand($query, $conn)
+$command.ExecuteScalar()
+$conn.Close()
