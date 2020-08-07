fde5701cab746bdc1bcdb69c537dec58152be989
Mikael Weaver <mikael.weaver@microsoft.com>
2020-08-07 12:07:43 -0700

    Fixed push metrics to work on OSX

--- bash-push-custom-log-analytics/pushMetrics.sh
+++ bash-push-custom-log-analytics/pushMetrics.sh
@@ -12,11 +12,16 @@ buildSignature () {
     local xHeaders="x-ms-date:${date}"
     local string_to_sign="${method}\n${contentLength}\n${contentType}\n${xHeaders}\n${resource}"
     #printf "$string_to_sign"
-    
-    local decoded_hex_key="$(echo -n $sharedKey | base64 -d -w0 | xxd -p -c256)"
-    local signature=$(printf "$string_to_sign" | openssl dgst -sha256 -mac HMAC -macopt "hexkey:$decoded_hex_key" -binary | base64 -w0)
 
-    echo "SharedKey ${customerId}:${signature}"
+    if [ "$(uname)" == "Darwin" ]; then
+        local decoded_hex_key="$(echo -n $sharedKey | base64 -d | xxd -p -c256)"
+        local signature=$(printf "$string_to_sign" | openssl dgst -sha256 -mac HMAC -macopt "hexkey:$decoded_hex_key" -binary | base64)
+        echo "SharedKey ${customerId}:${signature}"
+    else
+        local decoded_hex_key="$(echo -n $sharedKey | base64 -d -w0 | xxd -p -c256)"
+        local signature=$(printf "$string_to_sign" | openssl dgst -sha256 -mac HMAC -macopt "hexkey:$decoded_hex_key" -binary | base64 -w0)
+        echo "SharedKey ${customerId}:${signature}"
+    fi
 }
 
 postLogAnalyticData() {
@@ -41,13 +46,17 @@ postLogAnalyticData() {
     "https://${customerId}.ods.opinsights.azure.com${resource}?api-version=2016-04-01"
 }
 
+# Log Analytics Workspace Information - Should be pushed in as env vars when not testing
 customerId="<Insert Log Analytics Customer Id>"
 sharedKey="<Insert Log Analytics Shared Key"
+
+# Data to post to custom table - should be JSON
 body="$(jq -n \
                   --arg type "CurrentTemp" \
                   --arg value 341 \
                   --arg date "$(TZ=GMT date "+%a, %d %h %Y %H:%M:%S %Z")" \
                   '{DataPointType:$type, DataPointValue:$value, DateValue:$date}')"
+# Name of custom table
 logType="WeatherType"
 timeStampField="DateValue"
 
